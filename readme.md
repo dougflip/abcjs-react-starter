95914143af6b0eab93138d8e1092bdb572d1fb55
dougflip <douglasdifilippo@gmail.com>
2020-12-05 12:44:57 -0500

    Initial commit with basic render

---
+++ readme.md
@@ -0,0 +1,23 @@
+A small app with
+[abcjs](https://github.com/paulrosen/abcjs)
+and
+[react-abcjs](https://github.com/rigobauer/react-abcjs)
+installed.
+
+## Setup
+
+Install Node - I recommend using nvm:
+
+```
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+```
+
+## Quickstart
+
+```
+npm install
+
+npm start
+```
+
+Browse to [http://localhost:1234/](http://localhost:1234/)
---
+++ src/songs.js
@@ -0,0 +1,12 @@
+export const songs = [
+  {
+    title: "Example",
+    notation:
+      "X:1\nT:Example\nM:4/4\nC:Trad.\nK:G\n|:Gccc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|",
+  },
+  {
+    title: "Example 2",
+    notation:
+      "X:1\nT:Dusty Miller, The\nT:Binny's Jig\nC:Trad.\nR:DH\nM:3/4\nK:G\nB>cd BAG|FA Ac BA|B>cd BAG|DG GB AG:|\nBdd gfg|aA Ac BA|Bdd gfa|gG GB AG:|\nBG G/2G/2G BG|FA Ac BA|BG G/2G/2G BG|DG GB AG:|",
+  },
+];
